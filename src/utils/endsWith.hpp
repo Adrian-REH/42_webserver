b9c7096c5428a70f8a28b0925d25c1dbae779c2e
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-01-22 13:51:35 +0100

    modified endsWith and startsWith functions

---
+++ src/utils/endsWith.hpp
@@ -0,0 +1,19 @@
+#ifndef ENDSWITH_HPP
+#define ENDSWITH_HPP
+#include <string>
+
+/**
+ * @brief Verifica si una cadena termina con un sufijo específico.
+ * 
+ * @param str Cadena principal.
+ * @param suffix Sufijo a comprobar.
+ * @return true Si la cadena termina con el sufijo.
+ * @return false En caso contrario.
+ */
+bool ends_with(const std::string& str, const std::string& suffix) {
+    if (str.size() < suffix.size()) {
+        return false;  // No puede terminar con el sufijo si es más pequeño
+    }
+    return str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+#endif
