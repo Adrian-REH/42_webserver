d3e589ab59768a765677a16db4f6ddb7c0b0693b
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-01-22 13:52:18 +0100

    extract ends_with of CGI_hpp

--- src/CGI.hpp
+++ src/CGI.hpp
@@ -1,23 +1,10 @@
 #ifndef CGI_HPP
 #define CGI_HPP
 #include <string>
+#include "utils/endsWith.hpp"
 #include <sys/wait.h>
 #include "utils/readFd.hpp"
 
-/**
- * @brief Verifica si una cadena termina con un sufijo específico.
- * 
- * @param str Cadena principal.
- * @param suffix Sufijo a comprobar.
- * @return true Si la cadena termina con el sufijo.
- * @return false En caso contrario.
- */
-bool ends_with(const std::string& str, const std::string& suffix) {
-    if (str.size() < suffix.size()) {
-        return false;  // No puede terminar con el sufijo si es más pequeño
-    }
-    return str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
-}
 
 /**
  * @brief Clase para gestionar la ejecución de scripts CGI.
