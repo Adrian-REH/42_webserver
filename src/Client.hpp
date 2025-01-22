86f46297a283d07b7fa3aebd6857b647f7c1bfdb
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-01-22 10:53:19 +0100

    Documentation added to client

--- src/Client.hpp
+++ src/Client.hpp
@@ -5,51 +5,109 @@
 #include <sys/epoll.h>
 #include <cstdlib>
 #include "Request.hpp"
-#include "Response.hpp"
+
+/**
+ * @brief Clase que representa un cliente conectado al servidor.
+ * 
+ * La clase `Client` encapsula la información y operaciones relacionadas con un cliente,
+ * como el socket asociado y la solicitud (`Request`) enviada por el cliente.
+ */
 class Client {
 private:
-    int _socket_fd;
+	int _socket_fd;
 	Request _request;
 
 public:
-    Client(int socket_fd) : _socket_fd(socket_fd), _request() {}
-    ~Client() {}
-	
-
-    // Método para recibir datos
-    bool receive_data() {
-        char buffer[1024];
-        int bytes_received = recv(_socket_fd, buffer, sizeof(buffer), 0);
-        if (bytes_received > 0) {
-            std::cout << "Parser Request "<< std::endl;
-			_request.parse_request(buffer, bytes_received);
-            return true;
-        } else {
-            // Cliente desconectado
-            std::cout << "Cliente desconectado, socket fd: " << _socket_fd << "\n";
-            close(_socket_fd);
-            return false;
-        }
-    }
+    /**
+     * @brief Constructor de la clase `Client`.
+     * 
+     * Inicializa un cliente con el descriptor de archivo del socket proporcionado
+     * y crea un objeto `Request` vacío.
+     * 
+     * @param socket_fd Descriptor de archivo del socket asociado al cliente.
+     */
+	Client(int socket_fd) : _socket_fd(socket_fd), _request() {}
+    /**
+     * @brief Destructor de la clase `Client`.
+     * 
+     * Libera los recursos asociados con el cliente. Actualmente no realiza
+     * ninguna acción explícita.
+     */
+	~Client() {}
+    /**
+     * @brief Obtiene la solicitud (`Request`) del cliente.
+     * 
+     * Este método devuelve una copia del objeto `Request` asociado al cliente.
+     * 
+     * @return Una copia del objeto `Request` del cliente.
+     */
 	Request get_request() const {
 		return _request;
 	}
-
-
-    // Método para enviar la respuesta
-    void send_response(std::string &response) {
-        if (!response.empty()) {
-            std::cout << response.substr(0, response.find("\n")) << std::endl;
-            send(_socket_fd, response.c_str(), response.size(), 0);
-		close(_socket_fd);
-        }
+	/**
+	 * @brief Obtiene el descriptor de archivo del socket asociado al cliente.
+	 * 
+	 * Este método devuelve el descriptor de archivo del socket (`_socket_fd`) 
+	 * que representa la conexión del cliente con el servidor.
+	 * 
+	 * @return El descriptor de archivo del socket asociado al cliente.
+	 */
+	int get_socket_fd() const {
+		return _socket_fd;
 	}
+	/**
+	 * @brief Método para recibir datos desde un socket y procesar la solicitud recibida.
+	 * 
+	 * Este método escucha datos provenientes del socket asociado a la instancia. 
+	 * Si se reciben datos, estos se procesan a través del método `parse_request` de la clase `_request`.
+	 * Si no se reciben datos (por ejemplo, el cliente se desconecta), el socket se cierra.
+	 * 
+	 * @return true Si se reciben datos correctamente y se procesan.
+	 * @return false Si el cliente se desconecta o no se reciben datos.
+	 */
+	bool receive_data() {
+		char buffer[1024];
+		int bytes_received = recv(_socket_fd, buffer, sizeof(buffer), 0);
+		if (bytes_received > 0) {
+			std::cout << "Parser Request "<< std::endl;
+			_request.parse_request(buffer, bytes_received);
+			return true;
+		} else {
+			// Cliente desconectado
+			std::cout << "Cliente desconectado, socket fd: " << _socket_fd << "\n";
+			close(_socket_fd);
+			return false;
+		}
+	}
+	/**
+	 * @brief Envía una respuesta al cliente a través del socket y cierra la conexión.
+	 * 
+	 * Este método toma una respuesta en forma de cadena, la envía al cliente utilizando
+	 * el socket asociado y luego cierra el socket. Si la respuesta no está vacía,
+	 * imprime en la consola la primera línea de la respuesta (hasta el primer salto de línea).
+	 * 
+	 * @param response Referencia a un objeto `std::string` que contiene la respuesta a enviar.
+	 */
+	void send_response(std::string &response) {
+		if (!response.empty()) {
+			std::cout << response.substr(0, response.find("\n")) << std::endl;
+			send(_socket_fd, response.c_str(), response.size(), 0);
+		close(_socket_fd);
+		}
+	}
+	/**
+	 * @brief Envía un mensaje de error HTTP al cliente y cierra la conexión.
+	 * 
+	 * Este método construye una respuesta HTTP de error con el código de estado proporcionado
+	 * y el mensaje correspondiente. Luego, envía la respuesta al cliente a través del socket
+	 * y cierra la conexión con el cliente.
+	 * 
+	 * @param code Código de error HTTP que indica el tipo de error (por ejemplo, 404, 500).
+	 * @param message Mensaje de error que describe el motivo del error (por ejemplo, "Not Found", "Internal Server Error").
+	 */
 	void send_error(int code, const std::string& message) {
 		std::string response = "HTTP/1.1 " + to_string(code) + " " + message + "\r\n\r\n";
 		send(_socket_fd, response.c_str(), response.size(), 0);
 		close(_socket_fd);
 	}
-    int get_socket_fd() const {
-        return _socket_fd;
-    }
 };
