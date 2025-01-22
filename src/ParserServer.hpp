0be87edccf62a1d5a731834e9b8ae50f836b195c
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-01-22 13:53:02 +0100

    Build ParserServer execute

--- src/ParserServer.hpp
+++ src/ParserServer.hpp
@@ -5,6 +5,7 @@
 #include "utils/readFileName.hpp"
 #include <string>
 #include <deque>
+#include <vector>
 
 class ParserServer {
 	private:
@@ -17,10 +18,20 @@ class ParserServer {
 		/**
 		 * @brief Busco en el archivo la configuracion necesaria para Server
 		 */
-		Server* execute() {
-			Server *srv = new Server();
-			srv->addLocation("", "");
-			return srv;
+		std::vector<Server> execute() {
+			std::vector<Server> srvs;
+			Location loc;
+
+			std::deque<std::string>::iterator it;
+			if (_content_file.size() <= 0)
+				throw std::runtime_error("Error not config");
+			for (it = _content_file.begin(); it != _content_file.end(); it++ ) {
+				//Busco http: std::vector<Server> srv
+				//----Busco server: srv.push_back()
+				//----------Busco Location
+			}
+			srvs.push_back(Server().addLocation(loc));
+			return srvs;
 		}
 };
 #endif
