49abe4eb1469e2034e9411c4c3ae8c7d57333ff1
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-01-22 19:51:34 +0100

    refactor name

--- src/utils/readFileName.hpp
+++ src/utils/readFileName.hpp
@@ -17,7 +17,7 @@ std::deque<std::string> readFileName(const char *_filename)
 	if (!inFile)
 		throw std::runtime_error("Error: could not open file");
 	while (std::getline(inFile, line))
-		content.push_back(line);
+		content.push_back(line.substr(0, line.find("#")));
 	inFile.close();
 	return content;
 }
