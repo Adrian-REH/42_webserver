99b36433c5fbc884f97596b3ccaa7c78a001d41b
Gracy <grace.ktaza@gmil.com>
2025-02-09 19:47:04 +0100

    CGI hpp to cpp

---
+++ src/CGI.cpp
@@ -0,0 +1,82 @@
+
+#include "CGI.hpp"
+
+
+CGI::CGI(const std::string& script_path, const std::string& method, const std::string& body, char** env)
+	: _script_path(script_path), _method(method), _body(body), _env(env) {}
+
+/**
+ * @brief Determina el intérprete adecuado para el script según su extensión.
+ * 
+ * @return Ruta al ejecutable del intérprete correspondiente.
+ * @throws std::runtime_error Si el tipo de script no está soportado.
+ */
+std::string CGI::determine_interpreter() const {
+	if (ends_with(_script_path, ".py")) {
+		return "/usr/bin/python3";
+	} else if (ends_with(_script_path, ".php")) {
+		return "/usr/bin/php";
+	} else if (ends_with(_script_path, ".js")) {
+		return "/usr/bin/node";
+	} else {
+		throw std::runtime_error("Unsupported script type: " + _script_path);
+	}
+}
+/**
+ * @brief Ejecuta el script CGI y devuelve su salida.
+ * 
+ * Crea un proceso hijo que ejecuta el script CGI utilizando el intérprete correspondiente.
+ * Captura la salida del script y la devuelve como una respuesta HTTP.
+ * 
+ * @return Respuesta HTTP generada por el script CGI.
+ * @throws std::runtime_error Si falla la creación del pipe o el fork.
+ */
+std::string CGI::execute() {
+	int status;
+	int io[2];
+	if (pipe(io) < 0) {
+		throw std::runtime_error("No se pudo crear un pipe.");
+	}
+
+	pid_t pid = fork();
+	if (pid < 0) {
+		throw std::runtime_error("Error al hacer fork.");
+	}
+
+	if (pid == 0) {
+		// Hijo: Configurar el entorno para la ejecución del CGI
+		close(io[0]);
+		dup2(io[1], STDOUT_FILENO);
+		dup2(io[1], STDERR_FILENO);
+		close(io[1]);
+		try {
+			// Determinar el intérprete según el tipo de script
+			std::string interpreter = determine_interpreter();
+
+			char* argv[] = {
+				(char*)interpreter.c_str(),
+				(char*)_script_path.c_str() + 1, // Quito le primer caracter '/'
+				(char*)_body.c_str(),
+				NULL
+			};
+			execve(interpreter.c_str(), argv, _env);
+			exit(EXIT_FAILURE);
+		} catch (const std::exception&  ) {
+			exit(2);
+		}
+	} else {
+		// Padre: Leer la salida del hijo
+		close(io[1]);
+		waitpid(pid, &status, 0);
+		status = WEXITSTATUS(status);
+		// TODO: Intentar devolver multiples codigos de error
+		if (status) {
+			close(io[0]);
+			std::string error(strerror(status));
+			throw std::runtime_error("Error en la ejecucion del CGI, Error : " + to_string(status) +  ", script_path: " + _script_path + ", body:" + _body);
+		}
+		std::string result = readFd(io[0]);
+		close(io[0]);
+		return (result);
+	}
+}
--- src/CGI.hpp
+++ src/CGI.hpp
@@ -1,10 +1,9 @@
 #ifndef CGI_HPP
 #define CGI_HPP
 
-#include <string>
+#include <cstring>
 #include <sys/wait.h>
 #include "utils/Utils.hpp"
-
 
 /**
  * @brief Clase para gestionar la ejecución de scripts CGI.
@@ -13,99 +12,40 @@
  * determinando el intérprete adecuado según el tipo de archivo.
  */
 class CGI {
-private:
-	std::string _script_path;
-	std::string _method;
-	std::string _body;
-	char** _env;
+	private:
+		std::string _script_path;
+		std::string _method;
+		std::string _body;
+		char** _env;
 
-public:
-	/**
-	 * @brief Constructor de la clase CGI.
-	 * 
-	 * @param script_path Ruta al script a ejecutar.
-	 * @param method Método HTTP asociado al script.
-	 * @param body Cuerpo de la solicitud HTTP.
-	 * @param env Variables de entorno para la ejecución.
-	 */
-	CGI(const std::string& script_path, const std::string& method, const std::string& body, char** env)
-		: _script_path(script_path), _method(method), _body(body), _env(env) {}
+	public:
+		/**
+		 * @brief Constructor de la clase CGI.
+		 * 
+		 * @param script_path Ruta al script a ejecutar.
+		 * @param method Método HTTP asociado al script.
+		 * @param body Cuerpo de la solicitud HTTP.
+		 * @param env Variables de entorno para la ejecución.
+		 */
+		CGI(const std::string& script_path, const std::string& method, const std::string& body, char** env);
+		/**
+		 * @brief Determina el intérprete adecuado para el script según su extensión.
+		 * 
+		 * @return Ruta al ejecutable del intérprete correspondiente.
+		 * @throws std::runtime_error Si el tipo de script no está soportado.
+		 */
+		std::string determine_interpreter() const;
 
-	/**
-	 * @brief Determina el intérprete adecuado para el script según su extensión.
-	 * 
-	 * @return Ruta al ejecutable del intérprete correspondiente.
-	 * @throws std::runtime_error Si el tipo de script no está soportado.
-	 */
-	std::string determine_interpreter() const {
-		if (ends_with(_script_path, ".py")) {
-			return "/usr/bin/python3";
-		} else if (ends_with(_script_path, ".php")) {
-			return "/usr/bin/php";
-		} else if (ends_with(_script_path, ".js")) {
-			return "/usr/bin/node";
-		} else {
-			throw std::runtime_error("Unsupported script type: " + _script_path);
-		}
-	}
-	/**
-	 * @brief Ejecuta el script CGI y devuelve su salida.
-	 * 
-	 * Crea un proceso hijo que ejecuta el script CGI utilizando el intérprete correspondiente.
-	 * Captura la salida del script y la devuelve como una respuesta HTTP.
-	 * 
-	 * @return Respuesta HTTP generada por el script CGI.
-	 * @throws std::runtime_error Si falla la creación del pipe o el fork.
-	 */
-	std::string execute() {
-		int status;
-		int io[2];
-		if (pipe(io) < 0) {
-			throw std::runtime_error("No se pudo crear un pipe.");
-		}
-
-		pid_t pid = fork();
-		if (pid < 0) {
-			throw std::runtime_error("Error al hacer fork.");
-		}
-
-		if (pid == 0) {
-			// Hijo: Configurar el entorno para la ejecución del CGI
-			close(io[0]);
-			dup2(io[1], STDOUT_FILENO);
-			dup2(io[1], STDERR_FILENO);
-			close(io[1]);
-			try {
-				// Determinar el intérprete según el tipo de script
-				std::string interpreter = determine_interpreter();
-
-				char* argv[] = {
-					(char*)interpreter.c_str(),
-					(char*)_script_path.c_str() + 1, // Quito le primer caracter '/'
-					(char*)_body.c_str(),
-					NULL
-				};
-				execve(interpreter.c_str(), argv, _env);
-				exit(EXIT_FAILURE);
-			} catch (const std::exception&  ) {
-				exit(2);
-			}
-		} else {
-			// Padre: Leer la salida del hijo
-			close(io[1]);
-			waitpid(pid, &status, 0);
-			status = WEXITSTATUS(status);
-			// TODO: Intentar devolver multiples codigos de error
-			if (status) {
-				close(io[0]);
-				std::string error(strerror(status));
-				throw std::runtime_error("Error en la ejecucion del CGI, Error : " + to_string(status) +  ", script_path: " + _script_path + ", body:" + _body);
-			}
-			std::string result = readFd(io[0]);
-			close(io[0]);
-			return (result);
-		}
-	}
+		/**
+		 * @brief Ejecuta el script CGI y devuelve su salida.
+		 * 
+		 * Crea un proceso hijo que ejecuta el script CGI utilizando el intérprete correspondiente.
+		 * Captura la salida del script y la devuelve como una respuesta HTTP.
+		 * 
+		 * @return Respuesta HTTP generada por el script CGI.
+		 * @throws std::runtime_error Si falla la creación del pipe o el fork.
+		 */
+		std::string execute();
 };
 
 #endif
