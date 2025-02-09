216f0e9bab511802141884d610ad9b3ba1d6a07a
Gracy <grace.ktaza@gmil.com>
2025-02-09 19:18:51 +0100

    Fixed utils for extracting strings

--- src/utils/extractStrBetween.cpp
+++ src/utils/extractStrBetween.cpp
@@ -18,7 +18,7 @@ std::string extractStrBetween(const std::string& line, const std::string& init, 
 std::string extractStrEnd(const std::string& line, const std::string& init) {
     size_t startPos = line.find(init);
     if (startPos == std::string::npos) {
-        throw std::invalid_argument("Start delimiter not found in line");
+        return line;
     }
     startPos += init.length(); // Mover el inicio justo después del delimitador inicial.
 
@@ -29,7 +29,7 @@ std::string extractStrEnd(const std::string& line, const std::string& init) {
 std::string extractStrStart(const std::string& line, const std::string& end) {
     size_t endPos = line.rfind(end);
     if (endPos == std::string::npos) {
-        throw std::invalid_argument("Start delimiter not found in line");
+        return "";
     }
 
     return line.substr(0, endPos);
@@ -46,9 +46,9 @@ std::string extractStrStart(const std::string& line, const std::string& end) {
 std::string extractStrREnd(const std::string& line, const std::string& end) {
     size_t endPos = line.rfind(end);
     if (endPos == std::string::npos) {
-        throw std::invalid_argument("Start delimiter not found in line");
+        return line;
     }
+        
     endPos += end.length();
-
     return line.substr(endPos, line.length());
 }
