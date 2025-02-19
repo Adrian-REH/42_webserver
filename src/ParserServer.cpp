a5dd18bde22aa79e30cb699017839d0e4c18cab7
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-02-19 12:11:31 +0100

    env is Fixed: unused, but  a FIXME was added about it

--- src/ParserServer.cpp
+++ src/ParserServer.cpp
@@ -1,7 +1,9 @@
 
 #include "ParserServer.hpp"
-
-
+/*
+typedef < typedef Function>
+std::map<std::string, Function> dictionaryParser
+*/
 LimitExcept ParserServer::parseLimitExcept(std::deque<std::string>::iterator &it, std::deque<std::string>::iterator end) {
 	LimitExcept limExc;
 	size_t lmtPos = it->find("limit_except ") + 13; // Salta "limit_except " (13 caracteres)
@@ -28,7 +30,7 @@ LimitExcept ParserServer::parseLimitExcept(std::deque<std::string>::iterator &it
 
 Location ParserServer::parseLocation(std::deque<std::string>::iterator &it, std::deque<std::string>::iterator end) {
 	Location loc;
-	loc.set_path(extractStrBetween(*it, "location ", "{"));
+	loc.set_path(extractStrBetween(*it, "location ", " {"));
 	for (++it; it != end; ++it) {
 		std::string line = (*it);
 		if (line.find("limit_except") != std::string::npos && line.find("{") != std::string::npos) {
@@ -38,6 +40,7 @@ Location ParserServer::parseLocation(std::deque<std::string>::iterator &it, std:
 			std::cout << line << std::endl;
 			return loc;
 		} else {
+			std::cout << line << std::endl;
 			if (line.find("index ") != std::string::npos && line.find(";") != std::string::npos){
 				loc.set_index(extractStrBetween(line, "index ", ";"));
 				continue;
@@ -92,6 +95,17 @@ Server ParserServer::parseServer(std::deque<std::string>::iterator &it, std::deq
 				//srv.set_server_name(extractStrBetween(line, "server_name ", ";"));
 				continue ;
 			}
+			if (line.find("keepalive_timeout ") != std::string::npos && line.find(";") != std::string::npos) {
+				//char *endp; TODO
+				//srv.set_server_name(extractStrBetween(line, "server_name ", ";"));
+				continue ;
+			}
+			if (line.find("keepalive_requestsz ") != std::string::npos && line.find(";") != std::string::npos) {
+				//char *endp; TODO
+				//srv.set_server_name(extractStrBetween(line, "server_name ", ";"));
+				continue ;
+			}
+			//TODO: Agregar los timeout de keep-alive
 			//std::cout << line << std::endl; // Procesar línea
 			//Identificar propiedades de Server y procesarlo. srv.set_propertie
 		}
@@ -119,56 +133,15 @@ int ParserServer::dumpRawData(const char *file_name)
 std::vector<Server> ParserServer::execute(char **env) {
 	std::vector<Server> srvs;
 	std::deque<std::string>::iterator it;
-
-	std::cout<< "env " << env[0] << std::endl; // TODO: borrar linea
-	//Verificar sintaxis y 
-	std::cout<< "n_lines of file: " << _content_file.size() << std::endl;
+	(void)env; // FIXME: Se precisa el env para cada ejecucion de CGI?, o es posible hacerlo sin utilizar el ENV que se envie, de igual forma se puede utilizar 
 	if (_content_file.size() <= 0)
 		throw std::runtime_error("Error not config");
 	for (it = _content_file.begin(); it != _content_file.end(); ++it) {
 		std::string line = (*it);
 		if (line.find("server ") != std::string::npos && line.find("{") != std::string::npos) {
 			std::cout << line << std::endl;
-			// srvs.push_back(parseServer(it, _content_file.end()));
-			parseServer(it, _content_file.end());
-			
+			srvs.push_back(parseServer(it, _content_file.end()));
 		}
 	}
-
-	srvs.push_back(Server()
-	.set_port(8080)
-	.addLocation(Location()
-		.set_path("/cgi-bin/")
-		.set_limit_except(LimitExcept()
-			.addAllowedMethod("GET")
-			.addAllowedMethod("POST")
-		)
-		.set_auto_index(true)
-		.set_root_directory("/cgi-bin")
-		.set_index("login.py")
-		.build()));
-	Server s;
-	s.set_port(8081);
-	s.addLocation(Location()
-		.set_limit_except(LimitExcept()
-			.addAllowedMethod("GET"))
-		.set_path("/")
-		.set_auto_index(true)
-		.set_root_directory("/html")
-		.set_index("index.html")
-		.build());
-	s.addLocation(Location()
-		.set_path("/cgi-bin/")
-		.set_auto_index(true)
-		.set_root_directory("/cgi-bin")
-		.set_index("login.py")
-		.build());
-	s.addLocation(Location()
-		.set_path("/images/")
-		.set_auto_index(true)
-		.set_root_directory("/img/www")
-		.build());
-	
-	srvs.push_back(s);
 	return srvs;
 }
