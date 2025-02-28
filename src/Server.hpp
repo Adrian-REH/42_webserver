0844e8d69155a62e535e7c86c98f415ea023b5d5
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-02-28 18:21:59 +0100

    The storage config responsibility was removed from Server and ServerConf was created

--- src/Server.hpp
+++ src/Server.hpp
@@ -19,58 +19,42 @@
 #include "LimitExcept.hpp"
 #include "Cookie.hpp"
 #include "utils/Utils.hpp"
-
-
+#include "SessionCookieManager.hpp"
+#include "Config.hpp"
 class Server {
 
 private:
 	int _port;
-	std::vector<Cookie> _cookies;
+	size_t _max_clients;
 	std::string _server_name;
 	std::map<int, Client*> _clients;
-	std::vector<Location> _locations;
-	std::map<int, std::string> _error_pages;
 	int _socket_fd;
-	size_t _max_clients;
-	size_t _timeout;
-	size_t _max_req;
-
-	//int epoll_fd;
-	//size_t _env_len;
-	
-	Cookie validate_session_id(std::string &session_id);
-	Cookie handle_cookie_session(std::string cookieHeader);
-	void execute(Client &client);
 
 public:
 	int handle_input_client(int client_fd);
 	int handle_output_client(int client_fd);
-	Server(int port = 8080, size_t max_clients = 1024, size_t timeout = 1, size_t max_req = 100);
+	Server(int port = 8080, size_t max_clients = 1024, std::string _server_name = "");
 	Server &set_port(const size_t port);
 	Server &setSocketFd(const int sock_fd);
 	Server &set_server_name(const std::string &server_name);
 	Server &setMaxClients(const int max_cients);
-	Server &set_error_page(const int code, std::string index);
-	Server &addLocation(const Location &location);
 	Server &addClient(const Client &cli);
-	Server &set_timeout(const size_t timeout);
-	Server &set_max_req(const size_t max_req);
-	void deleteClients();
-	std::string get_error_page_by_key(const int key);
-	std::vector<Location> get_locations();
 	int getSocketFd() const;
 	int getPort() const;
-	Client* get_client(int client_fd);
-	bool location_empty();
-	void deleteClient(const int client_fd);
 	int getMaxClients() const;
+	void deleteClients();
+	Client* get_client(int client_fd);
+	std::string get_server_name() const;
+
+	
+	void deleteClient(const int client_fd);
 	bool hasClientTimedOut(const int key_client_fd) {
+		ServerConfig srv_conf = Config::getInstance().getServerConfByServerName(_server_name);
 		std::map<int, Client*>::iterator it = _clients.find(key_client_fd);
 		if (it != _clients.end())
-			return it->second->has_client_timed_out() > _timeout;
+			return it->second->has_client_timed_out() > srv_conf.get_timeout();
 		return false;
 	}
-	std::string get_server_name() const;
 	std::pair <Server*, int> accept_connections(int epoll_fd);
 
 };
---
+++ src/ServerConfig.cpp
@@ -0,0 +1,126 @@
+#include "ServerConfig.hpp"
+#include "Logger.hpp"
+#include "Config.hpp"
+
+ServerConfig::ServerConfig(int port, size_t max_clients, size_t timeout, size_t max_req) : _port(port), _max_clients(max_clients), _timeout(timeout),_max_req(max_req) { //,_max_clients(max_clients), _env_len(0){
+}
+
+ServerConfig &ServerConfig::set_max_req(const size_t max_req) {
+
+	if (this->_max_req && max_req)
+		this->_max_req = (max_req > 100) ? 100: max_req;
+	return *this;
+}
+
+ServerConfig &ServerConfig::set_timeout(const size_t timeout) {
+	if (this->_timeout && timeout)
+	this->_timeout = (timeout > 5) ? 5: timeout;
+	return *this;
+}
+
+ServerConfig &ServerConfig::set_port(const size_t port) {
+
+	_port=port;
+	return *this;
+}
+
+ServerConfig &ServerConfig::set_server_name(const std::string &server_name) {
+	_server_name = server_name;
+	return *this;
+}
+
+ServerConfig &ServerConfig::setMaxClients(const int max_cients) {
+	_max_clients = max_cients;
+	return *this;
+}
+
+ServerConfig &ServerConfig::add_location(const Location &location) {
+	std::map<std::string, Location>::iterator it = _locations_conf.find(location.get_path());
+	if (it != _locations_conf.end())
+		throw ServerConfig::LocationRepeateException();
+	if (location.get_path().empty())
+		throw Location::LocationNotContainPathException();
+	_locations_conf[location.get_path()] = location;
+	return *this;
+}
+ServerConfig &ServerConfig::set_error_page(const int code, std::string path) {
+	std::map<int, std::string>::iterator it = _error_pages.find(code);
+	//TODO: Debo verificar si hay un archivo en ese path y si no hay lanzo un error
+	if (it != _error_pages.end())
+		_error_pages[code] = path;
+	return *this;
+}
+
+std::string ServerConfig::get_error_page_by_code(const int code) {
+	return _error_pages[code];
+}
+
+int countOccurrences(const std::string& str, const std::string& sub) {
+	int count = 0;
+	size_t pos = 0;
+
+	// Usamos find para buscar subcadenas
+	while ((pos = str.find(sub, pos)) != std::string::npos) {
+		count++;  // Encontró una coincidencia
+		pos += sub.length();  // Avanza la posición para buscar la siguiente coincidencia
+	}
+
+	return count;
+}
+
+/**
+ * @brief Busca la Location que coincide con el path dado, comprobando coincidencias exactas y prefijos.
+ * 
+ * Si se encuentra una coincidencia exacta, se devuelve esa Location. Si no, se buscan las Location que coincidan 
+ * con el prefijo más largo en la configuración y se devuelve la que tenga más coincidencias.
+ *
+ * @param path La ruta de la URL a comprobar.
+ * @return Location La Location que coincide con el path.
+ * @example prefixs:["/home/","/home/user/", "/"] path: "/home/user/pepe.py" -> return "/home/user/"
+ */
+Location ServerConfig::findMatchingLocation(const std::string path) {
+	std::map<std::string, Location>::iterator it = _locations_conf.find(path);
+	Location loc_ocurrence;
+	int tmp_max_ocurrences;
+	if (it == _locations_conf.end()) {
+		for (it = _locations_conf.begin(); it != _locations_conf.end(); it++) {
+			std::string prefix = it->first;
+			if (path.size() < prefix.size())
+				continue ;
+			int ocurrences = countOccurrences(path, prefix);
+			if ( ocurrences > tmp_max_ocurrences) {
+				tmp_max_ocurrences = ocurrences;
+				loc_ocurrence = it->second;
+			}
+		}
+		if (loc_ocurrence.empty())
+			throw std::runtime_error("There is no occurrence of location with the path sent");
+		return loc_ocurrence;
+	}
+	return it->second;
+}
+
+int ServerConfig::get_port() const{
+	return _port;
+}
+
+int ServerConfig::get_max_clients() const{
+	return _max_clients;
+}
+
+std::string ServerConfig::get_server_name() const {
+	return _server_name;
+}
+
+
+size_t ServerConfig::get_timeout() const {
+	return _timeout;
+}
+
+size_t ServerConfig::get_max_req()const {
+	return _max_req;
+}
+
+std::map<std::string, Location> ServerConfig::get_locations() {
+	return _locations_conf;
+}
