b3ebe323d8fa033926e95dc788be3827775be953
Adrian Herrera <adrianherrera.r.e@gmail.com>
2025-03-01 22:41:40 +0100

    Not Found added in Server

--- src/Server.cpp
+++ src/Server.cpp
@@ -153,6 +153,11 @@ int Server::handle_output_client(int client_fd) {
 		std::string val(e.what());
 		Logger::log(Logger::ERROR,"Server.cpp",  e.what());
 		return -1;
+	} catch (HttpException::NotFoundException &e) {
+		client->send_error(404, "Not Found");
+		std::string val(e.what());
+		Logger::log(Logger::ERROR,"Server.cpp",  e.what());
+		return -1;
 	} catch (HttpException::InternalServerErrorException &e) {
 		client->send_error(500, "Internal Server Error");
 		std::string val(e.what());
