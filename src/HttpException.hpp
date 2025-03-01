ec4d073117b24de0c4ffae16b40a578693b5b25c
Gracy <grace.ktaza@gmil.com>
2025-03-01 19:20:04 +0100

    Added exception error codes

--- src/HttpException.hpp
+++ src/HttpException.hpp
@@ -4,12 +4,7 @@
 
 class HttpException  {
 	public:
-	class NotAllowedMethodException : public std::exception {
-		public:
-			virtual const char* what() const throw() {
-				return "405 Not Allowed Method";
-			}
-	};
+
 	class NoContentException : public std::exception {
 		public:
 			virtual const char* what() const throw() {
@@ -17,37 +12,69 @@ class HttpException  {
 			}
 	};
 	
-	class NotFoundException : public std::exception {
-		public:
-			virtual const char* what() const throw() {
-				return "404 Not Found";
-			}
-	};
-	class BadRequestException : public std::exception {
-		public:
-			virtual const char* what() const throw() {
-				return "400 Bad Request";
-			}
-	};
-	
-	class InternalServerErrorException : public std::exception {
-		public:
-			virtual const char* what() const throw() {
-				return "500 Internal Server Error";
-			}
-	};
 	class MovedPermanentlyRedirectionException : public std::exception {
 		public:
 			virtual const char* what() const throw() {
 				return "301 Moved Permanently redirection";
 			}
 	};
+
+	class BadRequestException : public std::exception {
+		private:
+			std::string _message;
+
+		public:
+			BadRequestException(std::string message = ""): _message(message) {}
+			virtual const char* what() const throw() {
+				return ("400 Bad Request : " + _message).c_str();
+			}
+	};
+
+	class NotFoundException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "404 Not Found";
+			}
+	};
+
+	class NotAllowedMethodException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "405 Not Allowed Method";
+			}
+	};
+
+	class RequestTimeoutException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "408 Request Timeout";
+			}
+	};
+
 	class RequestEntityTooLargeException : public std::exception {
 		public:
 			virtual const char* what() const throw() {
 				return "413 Request Entity Too Large";
 			}
 	};
+
+	class InternalServerErrorException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "500 Internal Server Error";
+			}
+	};
+
+	class HTTPVersionNotSupportedException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "505 HTTP Version Not Supported";
+			}
+	};
+
+
+	
+
 };
 
 
