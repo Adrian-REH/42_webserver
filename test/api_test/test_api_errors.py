ffbc94833cb092d9931186e645f61d56822f089f
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-03-05 19:15:33 +0100

    204 is returned if body is not returned

--- test/api_test/test_api_errors.py
+++ test/api_test/test_api_errors.py
@@ -7,7 +7,23 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 BASE_URL = "http://localhost:8080/"
 
-def test_uri_file_extension():
-    response = requests.get(BASE_URL + "cgi-bin/login.py", data=data)
-    assert response.status_code == 200
-    assert "Upload a file" in response.text
+def test_uri_400():
+    data = {"username": "admin", "password": "123"}
+    response = requests.post(BASE_URL + "cgi-bin/login.py", data=data)
+    assert response.status_code == 400
+    assert "Login Form" in response.text
+    
+def test_uri_502():
+    response = requests.get(BASE_URL + "cgi-bin/error/error.py")
+    assert response.status_code == 502
+    assert "Bad Gateway" in response.text
+
+def test_uri_408():
+    response = requests.get(BASE_URL + "cgi-bin/error/whiletrue.py")
+    assert response.status_code == 408
+    assert "Request Timeout" in response.text
+
+def test_uri_404():
+    response = requests.get(BASE_URL + "cgi-bin/error/hola!")
+    assert response.status_code == 404
+    assert "Not Found" in response.text
